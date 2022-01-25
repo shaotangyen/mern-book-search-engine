6399f86063bcb7748d9f95968ef94c27825ff29f
Shao <shaotang.yen@gmail.com>
2022-01-25 19:41:50 +1100

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,11 +3,10 @@ const mongoose = require('mongoose');
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost/book-search-engine',
   {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  }
-);
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 module.exports = mongoose.connection;
